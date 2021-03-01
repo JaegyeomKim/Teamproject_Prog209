d9663c1c47a0912ab5b62a3b3faeb193dabf0504
Kay1215 <jaegyeom1215@gmail.com>
2021-03-01 10:58:23 -0800

    doing

---
+++ index.js
@@ -0,0 +1,139 @@
+let inforarry = [];
+let pathlist = [];
+
+// define a constructor to create User's information objects
+let inforObject = function (pName, pGender, pBirth, pEmail, pSituation, pSymptoms, pPath, pInformation) {
+    //this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
+    this.Name = pName;
+    this.Gender = pGender;
+    this.Birth = pBirth; 
+    this.Email = pEmail;
+    this.Situation = pSituation;
+    this.Symptoms = pSymptoms;
+    this.Path = pPath;
+
+    this.Information = pInformation;
+}
+
+
+
+inforarry.push(new inforObject(
+    'Kay', 
+    'Male', 
+    '1994.12.15', 
+    'kay1215@gmail.com', 
+    'General', 
+    'Fever, Cough', 
+    ["BellevueCollege",
+    "2021. 02. 10",
+    "AM 08:30",
+    "PM 2:30"],
+    "I am Kay"))
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+    createList()
+
+    // HealthCondition -> Submibutton -> Push informaion to inforarry
+    document.getElementById('submitButton').addEventListener("click", function() {
+
+        let symptoms = [];
+        $("input[name='symptoms']:checked").each(function(e){
+            var value = $(this).val();
+            symptoms.push(value);        
+        })   
+
+
+        $("input[name='path']").each(function(e){
+            var value = $(this).val()
+            pathlist.push(value);        
+        })
+
+        inforarry.push(new inforObject(
+            document.getElementById('userName').value, 
+            document.getElementById('gender').value, 
+            document.getElementById('birth').value, 
+            document.getElementById('email').value, 
+            document.getElementById('select-condition').value, 
+            symptoms,
+            pathlist,
+            document.getElementById('firstComment').value))
+
+        alert('Completed')
+        console.log(inforarry)
+        createList()
+
+    })
+
+    document.getElementById("buttonAdd").addEventListener('click', function() {
+        let addPathList = document.getElementById("path_list")
+        var form = document.createElement('form')
+
+        //let input = document.createElement('input')
+        //input.setAttribute("type", "text")
+        //input.setAttribute('name', 'path')
+        //input.setAttribute('value', "Add Location")
+        //form.appendChild(input)
+//
+        //addPathList.appendChild(form)
+
+
+
+        for(let i = 0; i < 4; i++){
+            if (i == 0) {
+                let input = document.createElement('input')
+                input.setAttribute("type", "text")
+                input.setAttribute('name', 'path')
+                input.setAttribute('value', "Add Location")
+                form.appendChild(input)
+            }
+            else if (i == 1) {
+                let input = document.createElement('input')
+                input.setAttribute("type", "date")
+                input.setAttribute('name', 'path')
+                form.appendChild(input)
+            }
+            else if (i == 2) {
+                let input = document.createElement('input')
+                input.setAttribute("type", "time")
+                input.setAttribute('name', 'path')
+                form.appendChild(input)
+            }
+            else if (i == 3) {
+                let input = document.createElement('input')
+                input.setAttribute("type", "time")
+                input.setAttribute('name', 'path')
+                form.appendChild(input)
+            }
+        }
+        addPathList.appendChild(form)
+    })
+
+
+
+});
+
+
+
+function createList() {
+    let PathList = document.getElementById('divPathList');
+    while (PathList.firstChild) {
+        PathList.removeChild(PathList.firstChild)
+    }
+
+    let ul = document.createElement("ul");
+
+    inforarry.forEach(function (element,) {
+        let li = document.createElement('li');
+        //li.classList.add('onePath');
+        li.innerHTML = "Location:  " + element.Path[0] + " Date: " + element.Path[1] + " From " + element.Path[2] + " To " + element.Path[3] + " Situation: " + element.Situation + " (" + element.Symptoms + ")"; 
+        ul.appendChild(li);
+    });
+
+    PathList.appendChild(ul);
+    }
+
+
+
